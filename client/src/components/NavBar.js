d268acfb75499087c7cb0c227e372899ce677e69
AndreyGOIT <ae.borga@gmail.com>
2023-09-03 17:16:13 +0300

    DevicePage & NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,13 +3,15 @@ import { Context } from "../index";
 import BootstrapNavBar from "react-bootstrap/NavBar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
-import { SHOP_ROUTE } from "../utils/consts";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import { Button } from "react-bootstrap";
 import { observer } from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
+  const history = useHistory();
   return (
     <BootstrapNavBar bg="dark" data-bs-theme="dark">
       <Container>
@@ -18,9 +20,18 @@ const NavBar = observer(() => {
         </NavLink>
         {user.isAuth ? (
           <Nav className="ml-auto" style={{ color: "white" }}>
-            <Button variant={"outline-light"}>Админ панель</Button>
-            <Button variant={"outline-light"} className="ml-2">
-              Войти
+            <Button
+              variant={"outline-light"}
+              onClick={() => history.push(ADMIN_ROUTE)}
+            >
+              Админ панель
+            </Button>
+            <Button
+              variant={"outline-light"}
+              className="ml-2"
+              onClick={() => history.push(LOGIN_ROUTE)}
+            >
+              Выйти
             </Button>
           </Nav>
         ) : (
