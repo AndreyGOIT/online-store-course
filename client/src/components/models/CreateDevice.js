dea255f515072099c38fc39f9f0c9ab71871cbf7
AndreyGOIT <ae.borga@gmail.com>
2023-09-04 09:11:17 +0300

    Update CreateDevice.js

--- client/src/components/models/CreateDevice.js
+++ client/src/components/models/CreateDevice.js
@@ -1,21 +1,88 @@
-import React from "react";
-import { Form, Modal, Button } from "react-bootstrap";
+import React, { useContext, useState } from "react";
+import {
+  Form,
+  Modal,
+  Button,
+  Dropdown,
+  FormControl,
+  Row,
+  Col,
+} from "react-bootstrap";
+import { Context } from "../..";
+import DropdownItem from "react-bootstrap/esm/DropdownItem";
 
 const CreateDevice = ({ show, onHide }) => {
+  const { device } = useContext(Context);
+  const [info, setInfo] = useState([]);
+
+  const addInfo = () => {
+    setInfo([...info, { title: "", description: "", number: Date.now() }]);
+  };
+  const removeInfo = (number) => {
+    setInfo(info.filter((i) => i.number !== number));
+  };
+
   return (
     <Modal show={show} onHide={onHide} centered>
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Добавить тип
+          Добавить устройство
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control
-            // value={value}
-            // onChange={(e) => setValue(e.target.value)}
-            placeholder={"Введите название типа"}
+          <Dropdown>
+            <Dropdown.Toggle className="mt-2 mb-2">
+              Выберите тип
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.types.map((type) => (
+                <DropdownItem key={type.id}>{type.name}</DropdownItem>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown>
+            <Dropdown.Toggle className="mt-2 mb-2">
+              Выберите бренд
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {device.brands.map((brand) => (
+                <DropdownItem key={brand.id}>{brand.name}</DropdownItem>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <FormControl
+            className="mt-3"
+            placeholder="Введите название устройства "
           />
+          <FormControl
+            className="mt-3"
+            placeholder="Введите стоимость устройства "
+            type="number"
+          />
+          <FormControl className="mt-3" type="file" />
+          <hr />
+          <Button variant={"outline-dark"} onClick={addInfo}>
+            Добавить новое свойство
+          </Button>
+          {info.map((i) => (
+            <Row className="mt-4" key={i.number}>
+              <Col md={4}>
+                <FormControl placeholder="Введите название свойства" />
+              </Col>
+              <Col md={4}>
+                <FormControl placeholder="Введите описание свойства" />
+              </Col>
+              <Col md={4}>
+                <Button
+                  variant={"outline-danger"}
+                  onClick={() => removeInfo(i.number)}
+                >
+                  Удалить
+                </Button>
+              </Col>
+            </Row>
+          ))}
         </Form>
       </Modal.Body>
       <Modal.Footer>
