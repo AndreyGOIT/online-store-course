e286dc874611ef10069fa9892eebf8a512521a8d
AndreyGOIT <ae.borga@gmail.com>
2023-09-05 11:38:55 +0300

    summary final

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -13,12 +13,24 @@ const Shop = observer(() => {
   useEffect(() => {
     fetchTypes().then((data) => device.setTypes(data));
     fetchBrands().then((data) => device.setBrands(data));
-    fetchDevices().then((data) => {
+    fetchDevices(null, null, 1, 3).then((data) => {
       device.setDevices(data.rows);
       device.setTotalCount(data.count);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    fetchDevices(
+      device.selectedType.id,
+      device.selectedBrand.id,
+      device.page,
+      2
+    ).then((data) => {
+      device.setDevices(data.rows);
+      device.setTotalCount(data.count);
+    });
+  }, [device.page, device.selectedType, device.selectedBrand]);
 
   return (
     <Container>
